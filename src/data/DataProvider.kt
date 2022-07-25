b5b8636c206e5a679c08e3f83cd42129eb5fc7ba
aneesulanwar <aneesulanwar@gmail.com>
2022-07-26 02:36:18 +0500

    minor change

--- src/data/DataProvider.kt
+++ src/data/DataProvider.kt
@@ -6,7 +6,7 @@ import models.Item
 class DataProvider {
     companion object {
         fun getDataProvider():HashMap<Int,Pair<Item,Double>>{
-            var dataProvider = hashMapOf<Int, Pair<Item, Double>>()
+            var dataProvider = hashMapOf<Int, Pair<Item, Double>>()//map to store (items,price) pair with the item id as key
             dataProvider[FoodItems.BIG_BREKKIE.foodId] = Pair(Item(FoodItems.BIG_BREKKIE.foodId,"Big Brekkie"),16.0)
             dataProvider[FoodItems.BRUSCHETTA.foodId] = Pair(Item(FoodItems.BIG_BREKKIE.foodId,"Bruschetta"),8.0)
             dataProvider[FoodItems.POACHED_EGGS.foodId] = Pair(Item(FoodItems.BIG_BREKKIE.foodId,"Poached Eggs"),12.0)
